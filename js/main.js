83d1056c6647a5ecd2435c8d57609cf44cbf01fa
freelancersazal <freelancersazal@gmail.com>
2023-03-05 16:49:58 +0600

    home page updated

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@ $(function () {
 
     "use strict";
 
+    //======MENU FIX========
+    if ($('.tf_main_manu').offset() != undefined) {
+        $(window).bind('scroll', function () {
+            if ($(window).scrollTop() > 40) {
+                $('.tf_main_manu').addClass('menu_fix');
+            } else {
+                $('.tf_main_manu').removeClass('menu_fix');
+            }
+        });
+    }
 
 
+
+    //======BARFILLER JS========
+    $(document).ready(function () {
+        $('#bar1').barfiller();
+    });
 
 
 
